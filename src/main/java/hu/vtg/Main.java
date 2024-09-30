235017cf2cb9917bca1983db52ebe98e2511aabb
vtg <vtg@gmail.com>
2024-09-30 09:40:08 +0200

    valami van

--- src/main/java/hu/vtg/Main.java
+++ src/main/java/hu/vtg/Main.java
@@ -2,7 +2,9 @@ package hu.vtg;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
@@ -12,7 +14,11 @@ public class Main {
         Connection conn = DriverManager.getConnection(url, user, pass);
         System.out.println("OK");
         String sql = "SELECT * FROM jarmuvek";
-
+        Statement stmt = conn.createStatement();
+        ResultSet rs = stmt.executeQuery(sql);
+        while(rs.next()) {
+            System.out.println(rs.getString("id") + "." + " " + rs.getString("rendszam") + " " + rs.getString("marka"));
+        }
         conn.close();
     }
 }
