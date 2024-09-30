e6ce2269441cd352807ef690e39eb715a61fc850
vtg <vtg@gmail.com>
2024-09-30 10:52:35 +0200

    kész

--- src/main/java/hu/vtg/Main.java
+++ src/main/java/hu/vtg/Main.java
@@ -2,17 +2,38 @@ package hu.vtg;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        String user = "test01";
-        String pass = "titok";
-        String url = "jdbc:mariadb://localhost:3306/test01";
-        Connection conn = DriverManager.getConnection(url, user, pass);
+        //(MYSQL) String user = "test01";
+        //(MYSQL) String pass = "titok";
+        //(MYSQL) String url = "jdbc:mariadb://localhost:3306/test01";
+        String url = "jdbc:sqlite:database.sqlite";
+        //(MYSQL) Connection conn = DriverManager.getConnection(url, user, pass);
+        Connection conn = DriverManager.getConnection(url);
         System.out.println("OK");
+        
+
+        String insertSql = 
+            """
+            insert into jarmuvek
+            (rendszam, marka)
+            values
+            (?, ?)        
+            """;
+        
+
+        PreparedStatement ps = conn.prepareStatement(insertSql);
+        ps.setString(1, "RDA-348");
+        ps.setString(2, "Opel"); 
+        ps.executeUpdate();      
+        
+        
+        
         String sql = "SELECT * FROM jarmuvek";
         Statement stmt = conn.createStatement();
         ResultSet rs = stmt.executeQuery(sql);
@@ -20,5 +41,7 @@ public class Main {
             System.out.println(rs.getString("id") + "." + " " + rs.getString("rendszam") + " " + rs.getString("marka"));
         }
         conn.close();
+
+    
     }
 }
